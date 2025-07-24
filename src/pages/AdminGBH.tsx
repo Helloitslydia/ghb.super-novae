43a420e4ea0c4d0c14360c3317dc1f5694e49f88
Lydia Mallet <99566423+Helloitslydia@users.noreply.github.com>
2025-07-24 10:21:49 +0200

    feat: simplify admin GBH application modal

--- src/pages/AdminGBH.tsx
+++ src/pages/AdminGBH.tsx
@@ -278,6 +278,7 @@ function AdminGBH() {
             isOpen={showDetails}
             onClose={() => setShowDetails(false)}
             application={selectedApplication}
+            simple
           />
         )}
       </div>
